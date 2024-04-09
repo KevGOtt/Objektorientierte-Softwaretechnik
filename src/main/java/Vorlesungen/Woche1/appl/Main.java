d1788319e55460bffa6ca16e13e5aa4c99ae1f15
Rafael Ulmer <rafael.ulmer@edu.fhdw.de>
2024-04-09 16:00:43 +0200

    hallo

--- src/main/java/Vorlesungen/Woche1/appl/Main.java
+++ src/main/java/Vorlesungen/Woche1/appl/Main.java
@@ -2,8 +2,6 @@ package Vorlesungen.Woche1.appl;
 
 public class Main {
     public static void main(String[] args) {
-        Autor autor = new Autor("abc", "abc" , "abc" , "abc");
-        System.out.println(autor);
     }
 
 }
