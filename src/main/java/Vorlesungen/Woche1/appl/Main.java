a89ced89be6bd34e3bc75fa9ea25460cd04ace1f
Kevin Szarafin <kevinszarafin@gmail.com>
2024-04-10 10:17:45 +0200

    10.04.2024 10:17

--- src/main/java/Vorlesungen/Woche1/appl/Main.java
+++ src/main/java/Vorlesungen/Woche1/appl/Main.java
@@ -1,7 +1,13 @@
 package Vorlesungen.Woche1.appl;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JInternalFrame frame = new JInternalFrame();
+        frame.setClosable(true);
+        frame.setSize(250,250);
+        frame.setVisible(true);
     }
 
 }
